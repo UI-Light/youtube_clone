da024cd1918a46c0283dd65fef27846f39457590
Aonani <Lightkarlton@gmail.com>
2022-05-30 04:36:02 -0700

    Add sliver appbar

--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -10,9 +10,52 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Container(
-      color: Colors.orange,
-    ));
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            leadingWidth: 150,
+            leading: const Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Image(
+                image: AssetImage('assets/youtube-logo.png'),
+              ),
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.cast,
+                  color: Colors.black,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.notifications_none_outlined,
+                  color: Colors.black,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.black,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(right: 8.0),
+                child: CircleAvatar(
+                  radius: 20,
+                  backgroundImage: AssetImage("assets/raya.png"),
+                ),
+              )
+            ],
+            floating: true,
+          )
+        ],
+      ),
+    );
   }
 }
